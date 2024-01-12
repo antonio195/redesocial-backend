1c82525a6561d19a562c26fef3129f4cecb5dde8
Antonio <antonio195br@gmail.com>
2024-01-12 13:11:35 -0300

    projeto base

---
+++ src/main/kotlin/com/redesocial/redesocialbackend/RedesocialBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.redesocial.redesocialbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RedesocialBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<RedesocialBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/redesocial/redesocialbackend/RedesocialBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.redesocial.redesocialbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedesocialBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
